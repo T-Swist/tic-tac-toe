b5684f5a47b9e0f5b18ff385dad95ea8c243a922
T-Swist <daiminahthompson@gmail.com>
2024-06-14 23:04:50 +0000

    All of my files

---
+++ script.js
@@ -0,0 +1,64 @@
+//gettting elements from the html
+let playBtn = document.getElementById("playBtn");
+let selectPlayerSec = document.querySelector(".seclet-player");
+let welcomeSec = document.querySelector(".welcome-sec");
+let onePlayer = document.getElementById("onePlayer");
+let twoPlayers = document.getElementById("twoPlayers");
+let playingAs = document.querySelector(".playing-as")
+let playerX = document.getElementById("playerX");
+let PlayerO = document.getElementById("PlayerO");
+let gamneBoard = document.querySelector(".game-board");
+
+//Getting the al of the nine btn on the game board
+let allGameBtn = document.querySelectorAll(".all-btn");
+allGameBtn.forEach( function (allGameBtn){
+  allGameBtn.innerHTML = playerX.value;
+});
+
+let btn1 = document.getElementById("1");
+let btn2 = document.getElementById("2");
+let btn3 = document.getElementById("3");
+let btn4 = document.getElementById("4");
+let btn5 = document.getElementById("5");
+let btn6 = document.getElementById("6");
+let btn7 = document.getElementById("7");
+let btn8 = document.getElementById("8");
+let btn9 = document.getElementById("9");
+
+//Removing the welcome section and displaying the second section
+playBtn.addEventListener("click", (e) => {
+  welcomeSec.style.setProperty("display", "none");
+  selectPlayerSec.style.removeProperty("display", "none");
+})
+
+
+//Removing the select player number section and displaying the third section
+onePlayer.addEventListener("click", (e) => {
+  selectPlayerSec.style.setProperty("display", "none");
+  playingAs.style.removeProperty("display", "none");
+})
+
+//Removing the select player number section and displaying the third section
+twoPlayers.addEventListener("click", (e) => {
+  selectPlayerSec.style.setProperty("display", "none");
+  playingAs.style.removeProperty("display", "none");
+})
+
+
+//Removing the play as x or o section and displaying the game board section
+playerX.addEventListener("click", (e) => {
+  playingAs.style.setProperty("display", "none");
+  gamneBoard.style.removeProperty("display", "none");
+  console.log(playerX.value)
+  
+})
+
+//Removing the play as x or o section and displaying the game board section
+PlayerO.addEventListener("click", (e) => {
+  playingAs.style.setProperty("display", "none");
+  gamneBoard.style.removeProperty("display", "none");
+  console.log(PlayerO.value)
+})
+
+
+
